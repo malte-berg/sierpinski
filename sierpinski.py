b06279fe68593adab79702fd85dd7b1191e288e5
Malte Berg <malte@malte.nu>
2023-03-27 14:09:40 +0200

    Added toggle for rainbow

--- sierpinski.py
+++ sierpinski.py
@@ -10,13 +10,15 @@ colors = [((255, 0, 0), (0, 69)),
           ((75, 0, 130), (69*5, 69*6)),
           ((148, 0, 211), (69*6, 69*7))]
 
-def rainbow(x):
+def changeColor(x):
     colorx = (x + 242)
     for i, (color, (start, end)) in enumerate(colors):
         if start <= colorx < end:
             t.pencolor(color)
             break
 
+# Set True for rainbow colored triangle
+rainbow = False
 
 # Initialize turtle
 t = turtle.Turtle()
@@ -44,7 +46,8 @@ for i in range(10000):
     # Midway points
     x, y = (pos[0]+p[r][0])/2, (pos[1]+p[r][1])/2
     t.goto(x, y)
-    rainbow(x)
+    if rainbow:
+        changeColor(x)
     t.dot()
 
 turtle.exitonclick()
